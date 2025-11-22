5e1f3847f2dd935c4d385344651c12ddd005f88b
cheng <kimichen2907@gmail.com>
2025-11-22 12:33:57 +0800

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,21 +7,26 @@ export default function Header(){
     return (
         <>
             <header className="header">
-                <div className="logo">GreenShop</div>
+                <div className="logo">
+                    GreenShop
+                </div>
+
                 <div className="search__bar">
-                <input type="text" placeholder="Search..." />
-                <i className="fa fa-search"></i>
+                    <input type="text" placeholder="Search..." />
+                    <i className="fa fa-search"></i>
                 </div>
 
                 <nav className="nav__links">
-                <a href="#product" className="nav__item">Products</a>
-                <div className="login__icon">
-                    <FaUserLarge size={16}></FaUserLarge>
-                </div>
-                <div className="cart__icon">
-                    <MdShoppingCart></MdShoppingCart>
-                    <span className="cart__count">0</span>
-                </div>
+                    <a href="#product" className="nav__item">Products</a>
+                    <div className="login__icon">
+                        <FaUserLarge size={16}></FaUserLarge>
+                    </div>
+
+                    <div className="cart__icon">
+                        <MdShoppingCart></MdShoppingCart>
+                        <span className="cart__count">0</span>
+                    </div>
+
                 </nav>
             </header>
         </>
